89ee121376928cc0e237631da4d48fdf7e36a2d9
Andr3yas <iacob2002andrei@gmail.com>
2020-12-10 16:42:17 +0200

    Added 3 buttons

--- Game/main.c
+++ Game/main.c
@@ -10,43 +10,48 @@ static void drawTitleScreen(void)
     int backHeight = GetScreenHeight() / 3;
     float roundness = 0.2f;
     int segments = 0;
-    Rectangle backRect = {GetScreenWidth() / 2 - backWidth / 2, 
-                          GetScreenHeight() / 2 - backHeight,
-                           backWidth, backHeight};
+    Rectangle backRect = {GetScreenWidth() / 2 - backWidth / 2, GetScreenHeight() / 2 - backHeight, backWidth, backHeight};
     DrawRectangleRounded(backRect, roundness, segments, GRAY);
 }
 #include <stdio.h>
-#define NUM_FRAMES 1
 
 int main(void)
 {
-    //No sound
-    //main.c:25:10: warning: variable 'btnAction' set but not used [-Wunused-but-set-variable]
-    //main.c:24:9: warning: variable 'btnState' set but not used [-Wunused-but-set-variable]
     
     InitWindow(WINWIDTH, WINHEIGHT, "Segmente");
 
     SetWindowMonitor (0);
-    SetTargetFPS(60); 
+    SetTargetFPS(60);
     
     // Upload placeholder image.
     InitAudioDevice();
     
     //Sound fxButton = LoadSound("Resources/");
     Texture2D button = LoadTexture("Resources/button.png");
+    Texture2D button2 = LoadTexture("Resources/sound.png");
+    Texture2D button3 = LoadTexture("Resources/settings.png");
     
     int frameHeight = button.height/NUM_FRAMES;
-    Rectangle sourceRec = {0,0,button.width,frameHeight};
+    int frameHeight2 = button2.height/NUM_FRAMES;
+    int frameHeight3 = button3.height/NUM_FRAMES;
     
-    Rectangle btnBounds = {WINWIDTH/0.85-button.width/2,WINHEIGHT/0.6-button.height/NUM_FRAMES/2,button.width,frameHeight};
+    Rectangle sourceRec = {0,0,button.width,frameHeight};
+    Rectangle btnBounds = {WINWIDTH/0.825-button.width/2,WINHEIGHT/0.85-button.height/2,button.width,frameHeight};
+    
+    Rectangle sourceRec2 = {0,0,button2.width,frameHeight2};
+    Rectangle btn2Bounds = {WINWIDTH/0.85-button2.width/2,WINHEIGHT/0.725-button2.height/2,button2.width,frameHeight2};
+    
+    Rectangle sourceRec3 = {0,0,button3.width,frameHeight3};
+    Rectangle btn3Bounds = {WINWIDTH/0.785-button3.width/2,WINHEIGHT/0.725-button3.height/2,button3.width,frameHeight3};
+    
     int btnState = 0;
     bool btnAction = false;
     Vector2 mousePoint = { 0.0f, 0.0f };
     
     struct Point pointArray[10]; 
-    setRandomPoints (pointArray, 10);          
+    setRandomPoints (pointArray, 10);
 
-    while (!WindowShouldClose())    
+    while (!WindowShouldClose())
     {
         mousePoint = GetMousePosition();
         btnAction = false;
@@ -62,18 +67,24 @@ int main(void)
         }
         else
             btnState = 0;
+        sourceRec.y = btnState*frameHeight;
+        //sourceRec.y = btnState*frameHeight2;
+
         
         BeginDrawing();
             drawTitleScreen();
             ClearBackground(RAYWHITE);
-            //for (int i = 1; i <= 10; i++) 
+            //for (int i = 1; i <= 10; i++)
                 //DrawCircle (pointArray[i].x, pointArray[i].y, 5, BLACK);
-            DrawTextureRec(button, sourceRec, (Vector2){btnBounds.x, btnBounds.y}, GREEN);
+            DrawTextureRec(button, sourceRec, (Vector2){btnBounds.x, btnBounds.y}, RAYWHITE);
+            DrawTextureRec(button2, sourceRec2, (Vector2){btn2Bounds.x, btn2Bounds.y}, RAYWHITE);
+            DrawTextureRec(button3, sourceRec3, (Vector2){btn3Bounds.x, btn3Bounds.y}, RAYWHITE);
         EndDrawing();
     }
     UnloadTexture(button);
+    UnloadTexture(button2);
     CloseAudioDevice();
-    CloseWindow();        
+    CloseWindow();
 
     return 0;
 }
--- Game/properties.h
+++ Game/properties.h
@@ -1,4 +1,5 @@
 #pragma once
 
 #define WINWIDTH 800
-#define WINHEIGHT 450
+#define WINHEIGHT 600
+#define NUM_FRAMES 1
